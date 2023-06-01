ab72270ece0b3c8e85f12c6bf32695edc3471817
ReinaldoJuunior <gamers.jrxiii@gmail.com>
2023-06-01 08:16:50 -0300

    Implementation hr-worker

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class HrWorkerApplication {
